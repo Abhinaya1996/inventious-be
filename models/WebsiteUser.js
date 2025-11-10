af6d130b248b980c9ea4c3cfe748173f102263a5
ABHINAYA R T <abhinaya.dev96@gmail.com>
2025-11-10 16:53:15 +0530

    Update WebsiteUser.js

--- models/WebsiteUser.js
+++ models/WebsiteUser.js
@@ -5,6 +5,7 @@ const websiteUserSchema = new mongoose.Schema({
   email: { type: String, unique: true },
   phone: { type: String },
   password: { type: String },
+  username: { type: String },
 
   brandName: { type: String },
   businessName: { type: String },
