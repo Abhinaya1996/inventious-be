de55cf0209aba98c729699217b9481f7dc92f67e
ABHINAYA R T <abhinaya.dev96@gmail.com>
2025-11-16 21:15:29 +0530

    Update server URL to production endpoint

--- swagger/swaggerWebsite.js
+++ swagger/swaggerWebsite.js
@@ -9,7 +9,7 @@ const websiteSwaggerOptions = {
       version: '1.0.0',
       description: 'Website user registration and authentication APIs',
     },
-    servers: [{ url: 'http://localhost:5001' }],
+    servers: [{ url: 'https://api.inventious.co/' }],
   },
   apis: ['./routes/websiteAuthRoutes.js'], // Website API routes
 };
